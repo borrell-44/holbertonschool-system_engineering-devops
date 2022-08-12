7fdf2972c6a2b6b9ec3a96dcafb1d7a513954101
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-12 15:59:03 -0400

    Fixing syntax

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # require   => Package['python-pip']
- # This source file will install flask from pip3
+# This source file will install flask from pip3
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
