db69cedb9added8edcd97274692459007d935fdd
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-12 12:19:35 -0400

    Comitting task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+file { '/tmp/school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
+  # This source file would be located on the Puppet master at
+  # /tmp/school
+}
