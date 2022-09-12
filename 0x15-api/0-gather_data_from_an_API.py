5729cb32e1d18885ac3da81e1ee67a2e5e1827ce
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-12 12:11:37 -0400

    Committing task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+from sys import argv
+import requests
+import json
+"""Rest API of todo list"""
+
+if __name__ == "__main__":
+    done = 0
+    total = 0
+    todo = []
+    arg = int(argv[1])
+    with requests.get('https://jsonplaceholder.typicode.com/users') as response:
+        data = response.text
+        parse_json = json.loads(data)
+
+    for u in parse_json:
+        if u.get("id") is arg:
+            with requests.get('https://jsonplaceholder.typicode.com/todos') as res:
+                d = res.text
+                parse = json.loads(d)
+
+                for task in parse:
+                    if task.get("userId") is arg:
+                        done += 1
+                        if task.get("completed") is True:
+                            todo.append(task.get("title"))
+                            total += 1
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(u.get("name"), total, done))
+            for line in todo:
+                print("\t{}".format(line))
