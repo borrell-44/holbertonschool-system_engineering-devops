af97e583c47d962db26f62949957d2f7afb59f05
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-29 12:30:48 -0400

    Test commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Get the number of subscribers from the Reddit API"""
+
+import json
+import requests
+
+def number_of_subscribers(subreddit):
+    """Return number of subs of the subreddit"""
+
+    url = "https://www.reddit.com/r/funny/about.json"
+    data = 'data'
+
+    val = requests.get(url).json()
+    return json.loads(val.data)
+
+    for k in d.keys():
+        if k == 'data':
+            print(d[k].get('subscribers'))
+
+    return 1
