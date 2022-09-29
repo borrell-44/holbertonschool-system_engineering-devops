24cc76214340ff81fda5bfc7138ab4f5cd40619b
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-29 13:08:45 -0400

    Making it more presentable

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,14 +15,7 @@ def number_of_subscribers(subreddit):
         val = requests.get(url).json()
     except json.decoder.JSONDecodeError:
         return 0
-
     if 'data' not in val.keys():
         return 0
 
     return val.get('data').get('subscribers')
-
-    for k in d.keys():
-        if k == 'data':
-            print(d[k].get('subscribers'))
-
-    return 1
