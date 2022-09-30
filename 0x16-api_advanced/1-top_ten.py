c4952fa75522186813eafe55c302010cebbdae75
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-29 20:02:21 -0400

    Committing task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Get the number of subscribers from the Reddit API"""
+
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """Return number of subs of the subreddit"""
+
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    header = {'User-Agent': 'Custom Agent',
+              'From': '4517@holbertonstudents.com'}
+
+    try:
+        val = requests.get(url, headers=header).json()
+    except json.decoder.JSONDecodeError:
+        print(None)
+        return
+    if val.get('error') == 404:
+        print(None)
+        return
+
+    children = val.get('data').get('children')
+    for i in range(9):
+        if i > len(children):
+            return
+        print(children[i].get('data').get('title'))
