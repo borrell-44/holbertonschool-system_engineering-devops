9d616852c965e904d9158825a5eda0e769bb81aa
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-29 20:51:54 -0400

    Hot Fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-#Fix Apache returning a 500 error
+# Fix Apache returning a 500 error
 exec {'PHPSetting':
-  command  => 'sed -i "s/.phpp/g" /var/www/html/wp-settings.php',
+  command  => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
   provider => shell
 }
